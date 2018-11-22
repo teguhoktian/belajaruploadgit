5e49daf6dfec5c91784edb851c6a5f096d8ee067
teguhoktian <oktian89@gmail.com>
2018-11-22 10:15:30 +0700

    perubahan

---
+++ upload.php
@@ -0,0 +1,20 @@
+<script type="text/javascript" src="node_modules/jquery/dist/jquery.min.js"></script>
+<script type="text/javascript">
+    $.ajaxSetup({
+        headers: {
+          'X-CSRF-TOKEN': 'token-tokenan'
+        }
+      });
+</script>
+<script type="text/javascript">
+    
+    $(function(){
+        $.ajax({
+            url: 'http://127.0.0.1:8000/v2/api/user',
+            success: function(response){
+                console.log(response);
+            },
+            method: 'GET'
+        });
+    });
+</script>
